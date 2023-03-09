f037eb8070d6d46ecb8e4d2c6f6684e4fb45db5c
dmikomarov <77617042+dmikomarov@users.noreply.github.com>
2023-03-09 20:25:45 +0300

    добавил открытия браузера через селенид 1

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -13,6 +13,6 @@ public class TestBase {
     Configuration.baseUrl = "https://demoqa.com/";
     Configuration.browserSize = "1920x1080";
     Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-    //  Configuration.holdBrowserOpen = true;
+    //  Configuration.holdBrowserOpen = true1;
   }
 }
